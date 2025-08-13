483c343597fb13e4cb313ab19ee425420ab393bc
wmatava <wmatava@berkeley.edu>
2025-08-13 11:44:53 -0700

    Added setup.py to enable pip installing

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='HeST',
+    version='0.2.0',
+    packages=find_packages(),
+    install_requires=[ 'numpy', 'scipy', 'detprocess', 'numba'],
+    package_data={
+        'HeST': ['*.npy'],
+    }
+)
